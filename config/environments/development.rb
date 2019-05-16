1657ec4c2cd6281a330d85380ed5ca67fff7cc77
kitahara6 <kitahara@sackle.co.jp>
2019-05-16 14:54:06 +0900

    アカウント登録確認実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,18 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # default url
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "kitamori6@gmail.com ",
+    :password => "kitamori5",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, "4HLsgORkqy77KY2psXL2XX2uk", "6cnrigGv2OhCtlYZYesz5xodbHDr5Slggdq4OobJvIfKvRFFaD"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -296,4 +297,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # mail setting
+  config.mailer_sender = "kitamori6@gmail.com"
 end
