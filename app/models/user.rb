06f61e73c431f493937cc7204cc095ea68daed93
kitahara6 <kitahara@sackle.co.jp>
2019-05-16 20:48:21 +0900

    Userモデル追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
 
+  has_many :tasks, dependent: :destroy
+
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
       user.provider = auth["provider"]
