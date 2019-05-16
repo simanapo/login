6f8f3699f111431471b9ec05c020c27239f49974
kitahara6 <kitahara@sackle.co.jp>
2019-05-16 20:48:03 +0900

    ルーティング・スキーマコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
-  root 'pages#index'
-  get 'pages/show'
+  # root 'pages#index'
+  # get 'pages/show'
+  # get 'pages/search'
+
+  resources :pages, only: [:index, :show, :create, :update, :destroy] do
+    collection do
+      post :confirm
+      post :csv_upload
+      post :csv_load
+      get :search
+    end
+    member do
+      patch :sort
+      patch :update_confirm
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2019_05_16_024441) do
 
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "task_name", default: "", null: false
+    t.string "task_detail", default: "", null: false
+    t.integer "display_order", null: false
+    t.integer "old_flg", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
